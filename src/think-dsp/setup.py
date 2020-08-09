9caaa3c5095f3d5f33ec16bee07fb93c89b65208
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-09 09:45:05 -0400

    create minimal setup.py

---
+++ src/think-dsp/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thinkdsp",
+    version="2.0.0",
+    author="Allen B. Downey",
+    author_email="downey@allendowney.com",
+    packages=find_packages(),
+    url="https://github.com/AllenDowney/thinkdsp",
+)
